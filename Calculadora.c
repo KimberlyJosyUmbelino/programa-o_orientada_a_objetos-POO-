5bfc9cfe352b906f503ceb4ec9c0ce27c6af240c
Alex-Umbelino <luiz.alex.umbelino@gmail.com>
2020-02-25 16:42:08 -0300

    primeira etapa da calculadora

---
+++ Calculadora.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+ int n1=0,n2=0,s1=0,s2=0,m1=0,m2=0,s;
+ float S1,S2,S;
+ //soma//
+ printf("Digite o primeiro numero inteiro que sera somado:\n");
+ scanf("%d",&n1);
+ printf("Digite o segundo numero inteiro:\n");
+ scanf("%d",&n2);
+ s=n1+n2;
+ printf("O resultado da soma:%d\n",s);
+
+return 0;
+}
